712c705561735efcf91399d7ad2e8a4c7701ae62
MrNtex <ntexyt@gmail.com>
2023-12-26 15:27:39 +0100

    Added gizmos to raycaster

--- Assets/AgentMovement.cs
+++ Assets/AgentMovement.cs
@@ -5,25 +5,51 @@ using UnityEngine.AI;
 
 public class AgentMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     private NavMeshAgent navMeshAgent;
 
+    [SerializeField]
     private float maxRange;
 
     [SerializeField]
     private Transform target;
+
+    // To store the ray's direction for drawing Gizmos
+    private Vector3 lastRayDirection;
+
     void Start()
     {
-        navMeshAgent = GetComponent<NavMeshAgent>();   
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, target.position) < maxRange)
+        if (Vector3.Distance(transform.position, target.position) < maxRange)
         {
-            navMeshAgent.destination = target.position;
+            // Create a ray from the agent to the target
+            Vector3 direction = target.position - transform.position;
+            lastRayDirection = direction; // Store the direction for Gizmos
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit hit;
+
+            // Perform the raycast
+            if (Physics.Raycast(ray, out hit, maxRange))
+            {
+                Debug.Log(hit.transform);
+                // Check if the raycast hit the target
+                if (hit.transform == target)
+                {
+                    // Set the destination to the target's position
+                    navMeshAgent.destination = target.position;
+                }
+            }
         }
-        
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw the ray in the Scene view
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, lastRayDirection);
     }
 }
